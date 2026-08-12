3252914bc916f892f641497521f5a843215808a1
Bartosz Budzynowski <bartoszbud@MacBook-Pro-Bartosz.local>
2026-08-13 00:38:49 +0200

    feature(iam): add polices module

---
+++ infra/modules/iam/policies/variables.tf
@@ -0,0 +1,14 @@
+variable "policy_name" {
+  description = "Policy name"
+  type        = string
+}
+
+variable "policy_json" {
+  description = "JSON document containing policy body"
+  type        = string
+}
+
+variable "policy_description" {
+  description = "Policy description"
+  type        = string
+}
