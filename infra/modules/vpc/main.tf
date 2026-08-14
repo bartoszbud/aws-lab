365813a0a2001ba5feadd4dd5f8605e12784e532
Bartosz Budzynowski <bartoszbud@MacBook-Pro-Bartosz.local>
2026-08-15 00:37:40 +0200

    add public route and associations

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -55,3 +55,21 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.environment} VPC public route"
+  }
+}
+
+resource "aws_route_table_association" "public_associations" {
+  for_each       = aws_subnet.public_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
