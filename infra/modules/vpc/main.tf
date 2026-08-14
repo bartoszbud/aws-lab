9d96825b30e28ffa6d4d2dc776d888ead64fae97
Bartosz Budzynowski <bartoszbud@MacBook-Pro-Bartosz.local>
2026-08-14 23:17:56 +0200

    initialize vpc module

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Description = "VPC for ${var.environment} environment"
+  }
+}
+
---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "module_name" {
+  description = "Module name"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "VPC IP address"
+  type        = string
+}
